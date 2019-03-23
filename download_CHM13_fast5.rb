ec0b8fbcffb37bbd5b32465b63ab43b654d3a9fd
Ubuntu <ubuntu@ip-172-31-5-105.ap-northeast-1.compute.internal>
2019-03-23 05:30:45 +0000

    WIP

--- download_CHM13_fast5.rb
+++ download_CHM13_fast5.rb
@@ -1,19 +1,36 @@
 #! /usr/bin/ruby
 require 'parallel'
+require 'csv'
 
+filename = "links.csv"
+rawdata = CSV.read(filename, headers:false)
+links = Hash.new()
+rawdata.each{|i|
+  links[i[0].to_i] = i[1]
+}
+Parallel.each(1..links.size){|i|
+  
+
+}
+
+
+=begin
 if File.exist?("CHM13") != true
   `git clone https://github.com/nanopore-wgs-consortium/CHM13`
 end
 `mkdir -p CHM13_fast5`
 filename = "CHM13/README.md"
+links    = Hash.new()
 File.open(filename, "r"){ |fp|
   fp.each_line {|line|
     if line.include?("nanopore/fast5") && line.include?("md5")
-      downloadlink = line.split(" ")[2].split("\"")[1]
-      patrition    = downloadlink.split("/")[-1]
-      `wget #{downloadlink} -P CHM13_fast5/`
-      `rm CHM13_fast5/#{patrition}`
+      url = line.split(" ")[2].split("\"")[1]
+      id  = url.split("/")[-1].split(".")[0][-3, 3].to_i
+      links[id] = url
     end
   }
 }
-
+links.each{|l|
+  print l[0].to_s + ", " + l[1] + "\n"
+}
+=end
